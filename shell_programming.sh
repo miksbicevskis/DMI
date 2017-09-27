ea02d9aa31f85c249a9c15f6ae96b1630392ec3e
krisypon@inbox.lv <miks.bicevskis@gmail.com>
2017-09-27 11:48:08 +0300

    20170927_11_50

--- shell_programming.sh
+++ shell_programming.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
-NAME="Vaards Uzvaards"
-#echo $NAME
+#4.piemers
+echo "Tiek pildiits skripts:"$0
+#echo $0
+#echo $n
+echo "Skriptam nodoto argumentu skaits:" $#
+echo "Skriptam nodoti argumenti (attelosana/grupesana 1):" $*
+echo "Skriptam nodoti arguemnti (attelosana/grupesana 2):" $@
+#echo $?
+echo "Skriptam pieskirtais procesa numurs:" $$
+#echo $!
 
+#3.piemers
+NAME="Vaards Uzvaards"
+echo $NAME
+unset NAME
+echo $NAME
+
+#2.piemers
 #readonly NAME
 #echo $NAME
 #NAME="Uzvaards Vaards"
 #echo $NAME
 
-echo $NAME
-unset NAME
-echo $NAME
+#1.piemers
+#NAME="Vaards Uzvaards"
+#echo $NAME
 
